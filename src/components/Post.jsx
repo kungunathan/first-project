32dad5467308a7c613f74ef1fc26e21e2b876f6e
Nathaniel Kung'u <kungun78@gmail.com>
2025-03-13 17:56:17 +0300

    First Commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react-refresh/only-export-components */
+function Post(){
+    return(
+    <div>
+        <p>My Name is Nathaniel</p>
+        <p>React is awesome!</p>
+        
+    </div>
+    );
+}
+export default Post();
